8e157aa61d3a164c23d658638e12a862a589a087
Cambuchi <49548018+Cambuchi@users.noreply.github.com>
2022-01-10 07:25:55 -0800

    Adds media queries for mobile usage

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -13,6 +13,7 @@ const addCard = async (cardInfo, cardImageLink) => {
   const cardImage = new Image();
   cardImage.src = cardImageLink;
   cardImage.classList = 'card-image';
+  cardImage.style = '--img-ratio: 0.685';
   // container for card information
   const cardInfoContainer = document.createElement('div');
   cardInfoContainer.classList = 'card-info';
